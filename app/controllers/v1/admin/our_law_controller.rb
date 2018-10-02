1589026be05cc6fdd73f534369324ad9c3e6b10e
Amirhossein Jani <amir.jani500@gmail.com>
2018-10-02 10:51:26 +0330

    our law admin controller

--- app/controllers/v1/admin/our_law_controller.rb
+++ app/controllers/v1/admin/our_law_controller.rb
@@ -16,7 +16,48 @@ class V1::Admin::OurLawController < ApplicationController
 
   # =========================== store
   def store
+    if  not params[:title] or not params[:description]
+      render json: { error: "پارامتر ها را به صورت کامل بفرستید" } , status: 400
+      return
+    end
 
+    law = @current_user.our_laws.build(ourLawParams)
+    if law.save
+      render json: law , status: :ok
+    else
+      render json: law.errors , status: 404
+    end
+  end
+
+  # ============================= update
+  def update
+    law = OurLaw.last
+    if law.update(ourLawParams)
+      render json: law , status: :ok
+    else
+      render json: law.errors , status: 404
+    end
+  end
+
+  # ============================= delete
+  def delete
+    law = OurLaw.last
+    if  law
+      if law.delete
+        render json: { is_deleted: "true" } , status: 200
+      else
+        render json: { is_deleted: "false" , reason: law.errors } , status: 400
+      end
+    else
+      render json: { error: "قوانین مورد نظر یافت نشد" } , status: 404
+    end
+  end
+
+  # ============================= private methods
+  private
+
+  def ourLawParams
+    params.permit(:title , :description)
   end
 
 end
