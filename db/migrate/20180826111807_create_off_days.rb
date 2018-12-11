9088671659026df59e0637aae3288d764c34595a
Amirhossein Jani <amir.jani500@gmail.com>
2018-12-11 17:35:51 +0000

    merge

--- db/migrate/20180826111807_create_off_days.rb
+++ db/migrate/20180826111807_create_off_days.rb
@@ -4,7 +4,9 @@ class CreateOffDays < ActiveRecord::Migration[5.2]
 
       t.references :user, type: :uuid, null: false, index: true ,foreign_key: { on_delete: :cascade , on_update: :cascade }
       t.date :date , null:false
-      t.string :description , null: true
+      t.boolean :is_all_day , null:true 
+      t.time :start , null:true 
+      t.time :end , null:true  
 
       t.timestamps
     end
